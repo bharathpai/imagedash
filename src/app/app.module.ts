932bcb2fc20b74632f5c3c49c247558e2ec71a31
bharathpai <bharath.pai@cavliwireless.com>
2021-11-30 17:26:39 +0530

    Created Authentication and Dashboard Modules - Added Signup and Login Components in auth module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AuthModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
