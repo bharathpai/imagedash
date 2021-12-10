ceea0890576aa17c1910ec306480c85049123a7b
bharathpai <bharath.pai@cavliwireless.com>
2021-12-10 11:06:28 +0530

    Added Auth Guard to Dashboard Routing Module.

--- src/modules/dashboard/dashboard-routing.module.ts
+++ src/modules/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UploaderComponent } from './components/uploader/uploader.component';
+import { AuthGuard } from 'src/app/shared/auth.guard';
 const routes: Routes = [
-    { path: 'uploader', component: UploaderComponent },
+    { path: 'uploader', component: UploaderComponent, canActivate: [AuthGuard] },
 
 ];
 
