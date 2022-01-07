58ee6ec6b3679bffb7db2d07d73d9b109e184d8e
bharathpai <bharath.pai@cavliwireless.com>
2022-01-07 14:00:59 +0530

    Removed unecessary code.

--- src/modules/dashboard/components/list/list.component.ts
+++ src/modules/dashboard/components/list/list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { S3Client, ListObjectsCommand } from "@aws-sdk/client-s3"
 
 @Component({
   selector: 'app-list',
@@ -9,21 +8,7 @@ import { S3Client, ListObjectsCommand } from "@aws-sdk/client-s3"
 export class ListComponent implements OnInit {
 
   constructor() { }
-  readonly REGION = 'ap-south-1'
-  readonly s3Client = new S3Client({ region: this.REGION })
-  bucketParams = { Bucket: 'imagedash' }
 
-  run = async () => {
-    try {
-      let data = await this.s3Client.send(new ListObjectsCommand(this.bucketParams))
-      console.log('Success', data);
-
-    }
-    catch (err) {
-      console.log('Error', err);
-
-    }
-  }
 
   ngOnInit(): void {
   }
