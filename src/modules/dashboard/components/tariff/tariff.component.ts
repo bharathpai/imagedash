0c80ec8c35a1128dddb8a404563efb506b4c3d55
bharathpai <bharath.pai@cavliwireless.com>
2022-01-07 11:19:15 +0530

    Created a Component to load and view Tariff sheet.

---
+++ src/modules/dashboard/components/tariff/tariff.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Input } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
+
+@Component({
+  selector: 'app-tariff',
+  templateUrl: './tariff.component.html',
+  styleUrls: ['./tariff.component.scss']
+})
+export class TariffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  data: AOA = [[], []];
+  fileName: string = 'SheetJS.csv';
+  value: any
+  // @Input() sample: any
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const ab: ArrayBuffer = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(ab);
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      // console.log(XLSX.utils.sheet_to_html(ws, { editable: true }))
+      // console.log(this.data);
+      console.log();
+
+
+    };
+    reader.readAsArrayBuffer(target.files[0]);
+  }
+
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
+
+  update() {
+    console.log(this.value);
+
+  }
+}
