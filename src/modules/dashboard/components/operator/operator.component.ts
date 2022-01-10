72690c04ec590245372bb75ab7f8fc91f2b7fe77
bharathpai <bharath.pai@cavliwireless.com>
2022-01-10 17:15:29 +0530

    Added template for showing the zone_details array as a table. - Added logic for adding and removing the list elements from the array. - Added basic number validation for price field. - Added some styles to the Add button. - Some minor changes.

--- src/modules/dashboard/components/operator/operator.component.ts
+++ src/modules/dashboard/components/operator/operator.component.ts
@@ -7,13 +7,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./operator.component.scss']
 })
 export class OperatorComponent implements OnInit {
+  zone_details: any[] = [];
 
   constructor(private fb: FormBuilder) { }
   details: FormGroup = this.fb.group({
+    operator: [''],
     zone: ['', Validators.required],
-    price: ['', Validators.required]
+    price: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]]
   })
+
   ngOnInit(): void {
   }
 
+  // Function to add an element details to zone_details.
+  add() {
+    this.zone_details.push(this.details.value)
+  }
+
+  // Function to remove an item from zone_details.
+  remove(data) {
+    this.zone_details.splice(this.zone_details.indexOf(data), 1);
+  }
 }
