27b2d4f8306bb1c0435496e7da717c7a9bc86ab3
bharathpai <bharath.pai@cavliwireless.com>
2022-01-20 11:12:34 +0530

    Added Toast messages in Toast service. - Written code for making the Zone name unique in Operator component. - Some minor changes.

--- src/modules/dashboard/components/operator/operator.component.ts
+++ src/modules/dashboard/components/operator/operator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OperatorService } from 'src/app/services/operator.service';
+import { ToastService } from 'src/app/services/toast.service';
 @Component({
   selector: 'app-operator',
   templateUrl: './operator.component.html',
@@ -11,7 +12,7 @@ export class OperatorComponent implements OnInit {
   zone_data: any[] = [];
   btnText: any = ["Add", `<i class="bi bi-trash-fill"></i>`]
 
-  constructor(private fb: FormBuilder, private op: OperatorService) { }
+  constructor(private fb: FormBuilder, private op: OperatorService, private toast: ToastService) { }
   details: FormGroup = this.fb.group({
     network_operator: [''],
     zone_details: this.fb.group({
@@ -26,13 +27,21 @@ export class OperatorComponent implements OnInit {
 
   // Function to add an element details to zone_data.
   add() {
+    this.zone_data.forEach((res) => {
+      if (this.details.value.zone_details.zone_name == res.zone_details.zone_name) {
+        this.toast.unique()
+        throw new Error('Duplicate value')
+      } 
+    })
     this.zone_data.push(this.details.value)
     this.op.networkOperator.next(this.zone_data)
+    this.toast.rowAdd()
   }
 
   // Function to remove an item from zone_data.
   remove(data) {
     this.zone_data.splice(this.zone_data.indexOf(data), 1);
     this.op.networkOperator.next(this.zone_data)
+    this.toast.rowRemove()
   }
 }
